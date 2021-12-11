e619381060dcb499452d9d68d7f528d4d9882515
hegiwibowo <hegi.wibowo@gmail.com>
2021-12-11 23:12:25 +0700

    add project

---
+++ db_provision.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Update Repository Ubuntu Server"
+sudo apt update && sudo apt full-upgrade -y
+echo "======================="
+
+echo "Installation Database Server"
+sudo apt install mysql-server -y
+echo "Done!!!"
+echo "======================="
+
+echo "Configuration MySQL"
+sudo cp /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf.bak
+sudo sed -i "s/.*bind-address.*/bind-address = 10.10.17.11/" /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo service mysql stop
+sudo service mysql start
+echo "Done!!!"
+echo "======================="
+
+echo "Creating Database and User for Wordpress"
+sudo mysql << EOF
+CREATE DATABASE IF NOT EXISTS dbsosmed;
+CREATE DATABASE IF NOT EXISTS wordpress_db;
+CREATE USER IF NOT EXISTS 'devopscilsy'@'%' IDENTIFIED BY '1234567890';
+GRANT ALL PRIVILEGES ON *.* TO 'devopscilsy'@'%';
+FLUSH PRIVILEGES;
+EOF
+echo "Done!!!"
+echo "======================="
+
+echo "Import Database For Social-Media"
+git clone https://github.com/ibnuzamra/sosial-media.git
+sudo mysql -u devopscilsy -p1234567890 dbsosmed < sosial-media/dump.sql
+rm -rf sosial-media/
+echo "Done!!!"
+echo "========================"
---
+++ web_provision.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+echo "Update Repository Ubuntu"
+sudo apt update && sudo apt full-upgrade -y
+echo "Done!!!"
+echo "======================="
+
+echo "Installation Webserver NGINX"
+sudo apt install nginx -y
+echo "Done!!!"
+echo "========================"
+
+echo "Installation PHP"
+sudo apt install php-fpm php-mysql -y
+echo "Done!!!"
+echo "======================="
+
+echo "Creating Directory Project Deployment"
+sudo mkdir -p /var/www/welcome.local/html/
+sudo mkdir -p /var/www/iwordpress.local/html/
+sudo mkdir -p /var/www/isosmed.local/html/
+echo "Done!!!"
+echo "========================="
+
+echo "Set Ownership Direktori Server Blocks"
+sudo chown -R vagrant:vagrant /var/www/welcome.local/html/
+sudo chown -R vagrant:vagrant /var/www/iwordpress.local/html/
+sudo chown -R vagrant:vagrant /var/www/isosmed.local/html/
+echo "Done!!!"
+echo "========================="
+
+echo "Set Permission Folder /var/www/"
+sudo chmod -R 755 /var/www
+echo "Done!!!"
+echo "========================"
+
+# CLONE SOURCE CODE
+echo "Clone Source Code landing-page"
+git clone https://github.com/ibnuzamra/landing-page.git
+mv landing-page/* /var/www/welcome.local/html/
+rm -rf landing-page
+echo "Done!!!"
+echo "========================"
+ 
+echo "Clone WordPress"
+git clone https://github.com/ibnuzamra/WordPress.git
+mv WordPress/* /var/www/iwordpress.local/html/
+rm -rf WordPress
+sudo cp /var/www/iwordpress.local/html/wp-config-sample.php /var/www/iwordpress.local/html/wp-config.php
+sudo sed -i 's/database_name_here/wordpress_db/g' /var/www/iwordpress.local/html/wp-config.php
+sudo sed -i 's/username_here/devopscilsy/g' /var/www/iwordpress.local/html/wp-config.php
+sudo sed -i 's/password_here/1234567890/g' /var/www/iwordpress.local/html/wp-config.php
+sudo sed -i 's/localhost/10.10.17.11/g' /var/www/iwordpress.local/html/wp-config.php
+echo "Done!!!"
+echo "========================"
+
+echo "Clone sosial-media"
+git clone https://github.com/ibnuzamra/sosial-media.git
+mv sosial-media/* /var/www/isosmed.local/html/
+rm -rf sosial-media
+sed -i 's/localhost/10.10.17.11/g' /var/www/isosmed.local/html/config.php
+echo "Done!!!"
+echo "========================"
+
+echo "Configuration Nginx"
+sudo cp /vagrant/landing-page /etc/nginx/sites-available/landing-page
+sudo cp /vagrant/wordpress /etc/nginx/sites-available/wordpress
+sudo cp /vagrant/sosial-media /etc/nginx/sites-available/sosial-media
+sudo ln -s /etc/nginx/sites-available/landing-page /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/wordpress /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/sosial-media /etc/nginx/sites-enabled/
+echo "Done!!!"
+echo "========================"
+
+echo "Restart Service"
+sudo nginx -t
+sudo nginx -s reload
+sudo systemctl restart php7.4-fpm.service
+sudo systemctl restart nginx.service
+echo "Done!!!"
+echo "========================"
